89e01e0d7fecfd484d4c45cb2e108d79d7b64570
akash <akashsehgal2005@gmail.com>
2025-12-03 23:07:01 +0530

    initial commit

---
+++ class52/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("mousemove",function(elem){
+    console.log(elem.clientX,elem.clientY);
+    
+    document.body.style.setProperty("--x", elem.clientX + "px")
+    document.body.style.setProperty("--y", elem.clientY + "px")
+})
